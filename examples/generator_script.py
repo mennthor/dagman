7696bfd2933ae02259bf7afcd21e616a7b61ccf2
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-06-07 18:43:00 -0400

    Added generator test script

---
+++ examples/generator_script.py
@@ -0,0 +1,22 @@
+from dagman import dagman
+
+job_creator = dagman.DAGManJobCreator()
+
+
+name = "Testjob"
+exe = "python"
+exe_args = "test.py"
+njobs = 10
+job_args = {"seed": 10 + list(range(njobs)), "id": "run"}
+outfile = "./results/outfile.txt"
+local_job_dir = "./jobfiles"
+
+job_creator.create_job(
+    name=name,
+    exe=exe,
+    exe_args=exe_args,
+    njobs=njobs,
+    job_args=job_args,
+    outfile=outfile,
+    local_job_dir=local_job_dir,
+    makedirs=True)
