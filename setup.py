9b8ef575fc1b98c2701320df341bc89beefe8605
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-06-07 18:11:03 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="dagman",
+      version="0.1",
+      description="Module to create DAGMan job and submit files",
+      author="Thorben Menne",
+      author_email="mennthor@aol.com",
+      url="github.com/mennthor/dagman",
+      packages=["dagman"],
+      )
